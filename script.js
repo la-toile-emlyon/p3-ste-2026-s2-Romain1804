f4f385b28f11f67da7dd638a7a12df990da1d3a7
SamAY4207 <samuelayadi07@gmail.com>
2026-06-16 16:50:50 +0200

    1.6

---
+++ script.js
@@ -0,0 +1,14 @@
+  var swiper = new Swiper(".mySwiper", {
+      cssMode: true,
+       slidesPerView: 2,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+      autoplay: true
+    });
